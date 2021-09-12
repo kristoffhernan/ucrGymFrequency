2f0787af6138f5ab3dd609330af6b9519e75b291
Kristoffer Hernandez <kristoffhernan98@gmail.com>
2021-09-12 12:14:05 -0700

    added data and time when write weather and gym

--- writeGymWeatherData.py
+++ writeGymWeatherData.py
@@ -1,20 +1,26 @@
 from weather.weatherAPI import return_weather_results
-from gym.ucrGymScraper import clean_data
+from gym.ucrGymScraper import gym_data
 import csv
+from datetime import datetime
 
+date_time = datetime.now()
+
+# writing weather data to csv
 weather_data = return_weather_results()
-
 with open ('weather/weatherData.csv', 'a', newline='') as file:
     csv_writer = csv.writer(file)
     #csv_writer.writerow(['Temperature', 'Humidity', 'Pressure', 'Weather Report'])
 
-    csv_writer.writerow([weather_data['temperature'], weather_data['humidity'], weather_data['pressure'], weather_data['weather_report']])
+    csv_writer.writerow([weather_data['temperature'], weather_data['humidity'], weather_data['pressure'], weather_data['weather_report'], date_time])
 
     file.close()
 
-gym_data = clean_data()
 
+gym_data = gym_data()
+# adding date time to each nested list
+[ls.append(date_time) for ls in gym_data]
 
+# writing gym data to csv
 with open('gym/ucrGymScraperData.csv', 'a', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
 
